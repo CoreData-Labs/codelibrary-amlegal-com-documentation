802df4d1ad29a4fa9147abfdaf000a79b51f6739
Prajwal Koirala <273113883+PrajwalKoirala638@users.noreply.github.com>
2026-09-21 16:16:53 +0545

    Update uploader.sh

--- uploader.sh
+++ uploader.sh
@@ -12,7 +12,7 @@ function main() { # Define the main function that contains all script logic
 	# ---------------- Configuration ----------------
 
 	CHECK_INTERVAL_SECONDS=60     # Wait 60 seconds between each repository check
-	MIN_WAIT_SECONDS=21600        # Force push at least every 21600 seconds (6 Hour)
+	MIN_WAIT_SECONDS=43200        # Force push at least every 43200 seconds (12 Hour)
 	MIN_FILE_CHANGE_THRESHOLD=100 # Push early if 100+ files have changed
 
 	last_push_epoch=$(date +%s) # Store current time in seconds since Unix epoch
